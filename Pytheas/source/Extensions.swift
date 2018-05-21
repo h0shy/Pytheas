f3f609f0a05d4d65e56c957185546ec7274b2081
hoshy <stefan@hoschkara.com>
2018-05-21 17:07:56 +0200

    Delete Extensions

--- Pytheas/source/Extensions.swift
+++ Pytheas/source/Extensions.swift
@@ -1,39 +1,39 @@
 //
-//  Extensions.swift
-//  Pytheas
+////  Extensions.swift
+////  Pytheas
+////
+////  Created by Stefan Hoschkara on 25.03.18.
+////  Copyright © 2018 Stefan Hoschkara. All rights reserved.
+////
 //
-//  Created by Stefan Hoschkara on 25.03.18.
-//  Copyright © 2018 Stefan Hoschkara. All rights reserved.
+//import MapKit
 //
-
-import MapKit
-
-extension MKPolyline {
-
-    var pointAnnotations: [MKPointAnnotation] {
-        
-        var coordinates = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
-        getCoordinates(&coordinates, range: NSRange(location: 0, length: pointCount))
-        
-        return coordinates.map {
-            let point = MKPointAnnotation()
-            point.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
-            return point
-        }
-    }
-}
-
-extension MKPolygon {
-    
-    var pointAnnotations: [MKPointAnnotation] {
-        
-        var coordinates = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
-        getCoordinates(&coordinates, range: NSRange(location: 0, length: pointCount))
-        
-        return coordinates.map {
-            let point = MKPointAnnotation()
-            point.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
-            return point
-        }
-    }
-}
+//extension MKPolyline {
+//
+//    var pointAnnotations: [MKPointAnnotation] {
+//
+//        var coordinates = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
+//        getCoordinates(&coordinates, range: NSRange(location: 0, length: pointCount))
+//
+//        return coordinates.map {
+//            let point = MKPointAnnotation()
+//            point.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
+//            return point
+//        }
+//    }
+//}
+//
+//extension MKPolygon {
+//
+//    var pointAnnotations: [MKPointAnnotation] {
+//
+//        var coordinates = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
+//        getCoordinates(&coordinates, range: NSRange(location: 0, length: pointCount))
+//
+//        return coordinates.map {
+//            let point = MKPointAnnotation()
+//            point.coordinate = CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude)
+//            return point
+//        }
+//    }
+//}
